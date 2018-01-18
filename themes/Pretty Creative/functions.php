36f9fbea0f29d544e09e6325e71cf2eaff569bc3
Grayson Erhard <grayson@graphiclux.com>
2018-01-18 16:36:29 -0700

    fixed upsell mess, still related products showing up

--- themes/Pretty Creative/functions.php
+++ themes/Pretty Creative/functions.php
@@ -549,7 +549,7 @@ remove_action( 'woocommerce_before_shop_loop', 'woocommerce_result_count', 20 );
 remove_action( 'woocommerce_after_single_product_summary', 'woocommerce_output_product_data_tabs', 10 );
 
 // Remove Upsells From Their Default Position
-//remove_action( 'woocommerce_after_single_product_summary', 'woocommerce_upsell_display', 15 );
+remove_action( 'woocommerce_after_single_product_summary', 'woocommerce_upsell_display', 15 );
 
 
 
