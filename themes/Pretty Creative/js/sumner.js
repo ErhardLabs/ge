8a41626153ed9bcb66bcb1443405394098c4f4c3
Sumner <serhard2556@gmail.com>
2018-03-13 22:23:09 -0600

    Mobile style improvements for the nav search

--- themes/Pretty Creative/js/sumner.js
+++ themes/Pretty Creative/js/sumner.js
@@ -45,22 +45,4 @@ jQuery(document).ready(function() {
     lastScrollTop = st;
   }
 
-  // Nav Search box
-  $('nav input.aws-search-field').wrap("<span id='search' class='search_box'></span>")
-  $('nav input.aws-search-field').click(function() {
-    if($('nav input.aws-search-field').val() == ''){
-      $('.aws-search-result').hide();
-    }
-    $('nav #menu-header-right li:not(:last-child)').css('display', 'none');
-    $('.search_box').addClass('hidden');
-  });
-  $(document).click(function() {
-    if(!$(event.target).closest('nav input.aws-search-field').length) {
-      $('nav input.aws-search-field').val('');
-      $('nav #menu-header-right li:not(:last-child)').css('display', 'inline-block');
-      $('.search_box').removeClass('hidden');
-    }
-  });
-
-
 });
