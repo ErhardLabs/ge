b28fbff105dbb5ca6672448633dd3df7b4ea0287
Grayson Erhard <grayson@graphiclux.com>
2018-05-07 22:45:08 -0600

    fixed some checkout styles, more dynamic alerts for mailing list subscribers who are checking out, GDPR compliance, small polishes

--- plugins/sexy-single-product-page/assets/js/sexy-single-product.js
+++ plugins/sexy-single-product-page/assets/js/sexy-single-product.js
@@ -3,7 +3,7 @@ jQuery( document ).ready(function(){
   // Set Random image background for Lyrics div
   let imagesArray = [
     SSP_LOCAL_PHP.siteUrl + '/wp-content/uploads/sites/14/2016/10/Grayson-Erhard-Colorado-Guitarist-and-Singer-Songwriter-Hair.jpg',
-    SSP_LOCAL_PHP.siteUrl + '/wp-content/uploads/sites/14/2018/05/Grayson-Erhard-12.jpg'
+    SSP_LOCAL_PHP.siteUrl + '/wp-content/uploads/sites/14/2018/05/Grayson-Erhard-12.jpg',
     SSP_LOCAL_PHP.siteUrl + '/wp-content/uploads/sites/14/2018/05/Grayson-Erhard-15.jpg'
   ];
 
